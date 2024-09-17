d60507ac2da5462a6a47acd55c86687c45cb5682
jason-sj-lee <jason.981214@gmail.com>
2024-09-17 10:44:07 -0400

    fix: fix lint

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -123,7 +123,7 @@ export default function LoginPage() {
 
           <div className="mt-6 text-center">
             <p className="text-sm text-[#006D77]">
-              Don't have an account?{" "}
+              Don't have an account?
               <Link
                 href="/signup"
                 className="text-[#E29578] hover:underline font-semibold"
